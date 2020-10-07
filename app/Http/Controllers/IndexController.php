fdeaaa986891dc00a8dd4f87a7cb05debd457601
grigor-aramyan <grigori.aramyan@gmail.com>
2020-10-07 23:49:39 +0400

    add index page scaffold

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -8,6 +8,6 @@ class IndexController extends Controller
 {
     public function index(Request $request)
     {
-        return response()->json(['msg' => 'test']);
+        return view('index');
     }
 }
---
+++ resources/views/structure.blade.php
@@ -0,0 +1,302 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>NewsPrk Dark Version</title>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <link rel="icon" href="assets/img/icon/fabicon.png">
+
+    <link rel="stylesheet" href="{{ asset('css/plugins/bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/plugins/animate.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/plugins/fontawesome.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/plugins/modal-video.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/plugins/owl.carousel.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/plugins/slick.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/plugins/stellarnav.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/theme.css') }}">
+</head>
+
+@yield('content')
+
+<div class="space-70"></div>
+
+<div class="footer footer_area1 dark-2">
+<div class="container">
+<div class="cta">
+<div class="row">
+<div class="col-md-6 align-self-center">
+<div class="footer_logo logo">
+<a href="index.html">
+<img src="assets/img/logo/footer_logo.png" alt="logo">
+</a>
+</div>
+<div class="social2">
+<ul class="inline">
+<li><a href="index.html#"><i class="fab fa-instagram"></i></a>
+</li>
+<li><a href="index.html#"><i class="fab fa-facebook-f"></i></a>
+</li>
+<li><a href="index.html#"><i class="fab fa-youtube"></i></a>
+</li>
+<li><a href="index.html#"><i class="fab fa-instagram"></i></a>
+</li>
+</ul>
+</div>
+</div>
+<div class="col-md-6 col-lg-4 offset-lg-2 align-self-center">
+<div class="signup_form">
+<form action="index.html">
+<input class="signup" type="email" placeholder="Your email address">
+<input type="button" class="cbtn" value="sign up">
+</form>
+<p>We hate spam as much as you do</p>
+</div>
+</div>
+</div>
+</div>
+<div class="border_white"></div>
+<div class="space-40"></div>
+<div class="row justify-content-center">
+<div class="col-lg-8">
+<div class="row">
+<div class="col-sm-6 col-lg">
+<div class="single_footer_nav border_white_right">
+<h3 class="widget-title2">News categories</h3>
+<div class="row">
+<div class="col-lg-6">
+<ul>
+<li><a href="index.html#">Politics</a>
+</li>
+<li><a href="index.html#">Business</a>
+</li>
+<li><a href="index.html#">Technology</a>
+</li>
+<li><a href="index.html#">Science</a>
+</li>
+<li><a href="index.html#">Health</a>
+</li>
+<li><a href="index.html#">Sports</a>
+</li>
+<li><a href="index.html#">Entertainment</a>
+</li>
+</ul>
+</div>
+<div class="col-lg-6">
+<ul>
+<li><a href="index.html#">Education</a>
+</li>
+<li><a href="index.html#">Obituaries</a>
+</li>
+<li><a href="index.html#">Corrections</a>
+</li>
+<li><a href="index.html#">Education</a>
+</li>
+<li><a href="index.html#">Today’s Paper</a>
+</li>
+<li><a href="index.html#">Corrections</a>
+</li>
+<li><a href="index.html#">Foods</a>
+</li>
+</ul>
+</div>
+</div>
+</div>
+</div>
+<div class="col-sm-6 col-lg">
+<div class="single_footer_nav">
+<h3 class="widget-title2">Living</h3>
+<div class="row">
+<div class="col-lg-6">
+<ul>
+<li><a href="index.html#">Crossword</a>
+</li>
+<li><a href="index.html#">Food</a>
+</li>
+<li><a href="index.html#">Automobiles</a>
+</li>
+<li><a href="index.html#">Education</a>
+</li>
+<li><a href="index.html#">Health</a>
+</li>
+<li><a href="index.html#">Magazine</a>
+</li>
+<li><a href="index.html#">Weddings</a>
+</li>
+</ul>
+</div>
+<div class="col-lg-6">
+<ul>
+<li><a href="index.html#">Classifieds</a>
+</li>
+<li><a href="index.html#">Photographies</a>
+</li>
+<li><a href="index.html#">NYT Store</a>
+</li>
+<li><a href="index.html#">Journalisms</a>
+</li>
+<li><a href="index.html#">Public Editor</a>
+</li>
+<li><a href="index.html#">Tools & Services</a>
+</li>
+<li><a href="index.html#">My Account</a>
+</li>
+</ul>
+</div>
+</div>
+</div>
+</div>
+</div>
+<div class="space-40"></div>
+<div class="border_white"></div>
+<div class="space-40"></div>
+<div class="row">
+<div class="col-sm-6 col-lg-5">
+<div class="single_footer_nav border_white_right">
+<h3 class="widget-title2">Opinion</h3>
+<div class="row">
+<div class="col-lg-6">
+<ul>
+<li><a href="index.html#">Today’s Opinion</a>
+</li>
+<li><a href="index.html#">Op-Ed Contributing</a>
+</li>
+<li><a href="index.html#">Contributing Writers</a>
+</li>
+<li><a href="index.html#">Business News</a>
+</li>
+<li><a href="index.html#">Collections</a>
+</li>
+<li><a href="index.html#">Today’s Paper</a>
+</li>
+<li><a href="index.html#">Saturday Review</a>
+</li>
+<li><a href="index.html#">Product Review</a>
+</li>
+</ul>
+</div>
+</div>
+</div>
+</div>
+<div class="col-sm-6 col-lg-7">
+<div class="twitter_feeds">
+<h3 class="widget-title2">Twitter feed</h3>
+<div class="single_twitter_feed border_white_bottom">
+<div class="twitter_feed_icon"> <i class="fab fa-twitter"></i>
+</div>
+<h6>Cyber Monday Sale, Save 33% on Jannah theme during our year-end Sale, Purchase a new license for your next project… <span>@newspark #technology https://dribbble.com/subash_chandra</span></h6>
+<p>March 26, 2020</p>
+</div>
+<div class="single_twitter_feed">
+<div class="twitter_feed_icon"> <i class="fab fa-twitter"></i>
+</div>
+<h6>Cyber Monday Sale, Save 33% on Jannah theme during our year-end Sale, Purchase a new license for your next project… <span>@newspark #technology https://dribbble.com/subash_chandra</span></h6>
+<p>March 26, 2020</p>
+</div>
+<div class="single_twitter_feed">
+<div class="twitter_feed_icon"> <i class="fab fa-twitter"></i>
+</div>
+<h6>Cyber Monday Sale, Save 33% on Jannah theme during our year-end Sale, Purchase a new license for your next project… <span>@newspark #technology https://dribbble.com/subash_chandra</span></h6>
+<p>March 26, 2020</p>
+</div>
+</div>
+</div>
+</div>
+</div>
+<div class="col-lg-4">
+<div class="extra_newss border_white_left pl-4">
+<h3 class="widget-title2">More news</h3>
+<div class="single_extra_news border_white_bottom">
+<p>TECHNOLOGY <span> / March 26, 2020</span>
+</p> <a href="index.html#">Nancy zhang a chinese busy woman and dhaka</a>
+<span class="news_counter">1</span>
+</div>
+<div class="single_extra_news border_white_bottom">
+<p>TECHNOLOGY <span> / March 26, 2020</span>
+</p> <a href="index.html#">Nancy zhang a chinese busy woman and dhaka</a>
+<span class="news_counter">2</span>
+</div>
+<div class="single_extra_news border_white_bottom">
+<p>TECHNOLOGY <span> / March 26, 2020</span>
+</p> <a href="index.html#">Nancy zhang a chinese busy woman and dhaka</a>
+<span class="news_counter">3</span>
+</div>
+<div class="single_extra_news border_white_bottom">
+<p>TECHNOLOGY <span> / March 26, 2020</span>
+</p> <a href="index.html#">Nancy zhang a chinese busy woman and dhaka</a>
+<span class="news_counter">4</span>
+</div>
+<div class="single_extra_news">
+<p>TECHNOLOGY <span> / March 26, 2020</span>
+</p> <a href="index.html#">Nancy zhang a chinese busy woman and dhaka</a>
+<span class="news_counter">5</span>
+</div>
+<div class="space-40"></div>
+<div class="border_white_bottom"></div>
+<div class="space-40"></div>
+<div class="footer_contact">
+<h3 class="widget-title2">Newspark news services</h3>
+<div class="single_fcontact">
+<div class="fcicon">
+<img src="assets/img/icon/mobile.png" alt="">
+</div> <a href="index.html#">On your mobile</a>
+</div>
+<div class="single_fcontact">
+<div class="fcicon">
+<img src="assets/img/icon/speacker.png" alt="">
+</div> <a href="index.html#">On smart speakers</a>
+</div>
+<div class="single_fcontact">
+<div class="fcicon">
+<img src="assets/img/icon/evelope.png" alt="">
+</div> <a href="index.html#">Contact Newspark news</a>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+<div class="copyright">
+<div class="container">
+<div class="row">
+<div class="col-lg-6 align-self-center">
+<p>&copy; Copyright 2020, Armcoding</p>
+</div>
+<div class="col-lg-6 align-self-center">
+<div class="copyright_menus text-right">
+<div class="language"></div>
+<div class="copyright_menu inline">
+<ul>
+<li><a href="about.html">About</a>
+</li>
+<li><a href="index.html#">Advertise</a>
+</li>
+<li><a href="index.html#">Privacy & Policy</a>
+</li>
+<li><a href="contact.html">Contact Us</a>
+</li>
+</ul>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+
+
+<script src="{{ asset('js/plugins/jquery.2.1.0.min.js') }}"></script>
+<script src="{{ asset('js/plugins/bootstrap.min.js') }}"></script>
+<script src="{{ asset('js/plugins/jquery.nav.js') }}"></script>
+<script src="{{ asset('js/plugins/jquery.waypoints.min.js') }}"></script>
+<script src="{{ asset('js/plugins/jquery-modal-video.min.js') }}"></script>
+<script src="{{ asset('js/plugins/owl.carousel.js') }}"></script>
+<script src="{{ asset('js/plugins/popper.min.js') }}"></script>
+<script src="{{ asset('js/plugins/circle-progress.js') }}"></script>
+<script src="{{ asset('js/plugins/wow.min.js') }}"></script>
+<script src="{{ asset('js/plugins/slick.min.js') }}"></script>
+<script src="{{ asset('js/plugins/stellarnav.js') }}"></script>
+<script src="{{ asset('js/main.js') }}"></script>
+</body>
+</html>
